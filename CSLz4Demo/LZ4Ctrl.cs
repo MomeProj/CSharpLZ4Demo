001c70503e2eeadcffd1797ffb227e629797ca48
mome <mome.huang@gmail.com>
2023-12-10 19:34:11 +0800

    init

---
+++ CSLz4Demo/LZ4Ctrl.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using K4os.Compression.LZ4;
+using K4os.Compression.LZ4.Streams;
+
+namespace CSLz4Demo
+{
+	public class LZ4Ctrl
+	{
+		public static byte[] Lz4Pack(byte[] in_data)
+		{
+			using (var data_dst = new MemoryStream())
+			{
+				using (var src = new MemoryStream(in_data))
+				using (var dst = LZ4Stream.Encode(data_dst))
+				{
+					src.CopyTo(dst);
+				}
+
+				return data_dst.ToArray();
+			}
+		}
+
+		public static byte[] Lz4Unpack(byte[] in_data)
+		{
+			using (var data_dst = new MemoryStream())
+			{
+				using (var in_stream = new MemoryStream(in_data))
+				using (var decoder = LZ4Stream.Decode(in_stream))
+				{
+					decoder.CopyTo(data_dst); ;
+				}
+
+				return data_dst.ToArray();
+			}
+		}
+	}
+}
---
+++ CSLz4Demo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using K4os.Compression.LZ4;
+using K4os.Compression.LZ4.Streams;
+
+namespace CSLz4Demo
+{
+	class Program
+	{
+		static string BytesArrayToString(byte[] data)
+		{
+			var sb = new StringBuilder("new byte[] { ");
+
+			foreach (var b in data)
+			{
+				sb.Append(b + ", ");
+			}
+
+			sb.Append("}");
+			return sb.ToString();
+		}
+		static void Main(string[] args)
+		{
+			{
+				var in_string = "1234567890_1234567890_1234567890_1234567890";
+				var in_bytes = Encoding.ASCII.GetBytes(in_string);
+				var out_bytes = LZ4Ctrl.Lz4Pack(in_bytes);
+				Console.WriteLine(BytesArrayToString(out_bytes));
+				Console.WriteLine($"out_bytes len: {out_bytes.Length}");
+			}
+
+			Console.WriteLine();
+			Console.WriteLine();
+
+			{
+				var in_bytes = new byte[] { 4, 34, 77, 24, 100, 112, 185, 32, 0, 0, 0, 185, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 95, 11, 0, 0, 11, 0, 0, 22, 0, 176, 95, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 0, 0, 0, 0, 153, 61, 16, 238, };
+				var out_bytes = LZ4Ctrl.Lz4Unpack(in_bytes);
+				Console.WriteLine(BytesArrayToString(out_bytes));
+				Console.WriteLine($"out_bytes string: {Encoding.UTF8.GetString(out_bytes)}");
+				Console.WriteLine($"out_bytes len: {out_bytes.Length}");
+			}
+		}
+	}
+}
